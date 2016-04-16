a9d99e1886cdd151e18530bed14c1c531c92a793
AungThiha <mr.aungthiha@gmail.com>
2016-04-16 18:22:02 +0630

    list files in root route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readdir('/home/ubuntu/movies/terminatorTvSeries', function(err, files){
+    if(err){
+      return res.send("Error ls");
+    }
+    res.render('index', { title: 'Terminator TV series', files: files });
+  });
+
 });
 
 module.exports = router;
